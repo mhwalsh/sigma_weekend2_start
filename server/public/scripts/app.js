896cc45f7ca2faf1d120962d6b1384638118f099
Millie Walsh <millie@primeacademy.io>
2016-11-07 09:54:50 -0600

    first pass with data

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -3,7 +3,12 @@ $(document).ready(function(){
       type: "GET",
       url: "/data",
       success: function(data){
-        console.log(data);
+        console.log(data.sigmanauts);
+        // get something on the dom
+        var students = data.sigmanauts;
+        for (var i = 0; i < students.length; i++) {
+          $("#outputDiv").append(students[i].name);
+        }
       }
     });
 });
